0ca533e4f6c518a383d81f9625ca39c42cedc64d
wdalton27 <wdalton27@students.stab.org>
2025-01-15 08:57:16 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Science/Rollercoasters.csv"
+
+var name = getColumn(url, 1);
+var ParkName = getColumn(url, 2);
+var City = getColmn(url, 3);
+var Country = getColmn(url, 4);
+var Region = getColmn(url, 5);
+var Material = getColmn(url, 6);
+var Height = getColmn(url, 7);
+var Speed = getColmn(url, 8);
+var Length = getColmn(url, 9);
+var Inversion = getColmn(url, 10);
+var NumInversion = getColmn(url, 11);
+var YearOpen = getColmn(url, 12);
+
+//List Coasters greater than 10 ft
+function getHeight(name){
+if ()
+}
+
+
+
+//List of Coasters in Germany
+
+
+
+
+//List of Coasters that opened after 2000
+
+
+
+
+//List of Coasters in at Six Flags Great America
+
+
+
+
+//List of Coasters that are made out of Wood
+
